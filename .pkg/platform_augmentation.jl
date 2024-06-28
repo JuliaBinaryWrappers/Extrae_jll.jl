ad4f03be472200e8a2cc5992fefbd95366750b98
jlbuild <juliabuildbot@gmail.com>
2024-06-28 17:13:10 +0000

    Extrae_jll build 4.2.0+0

--- .pkg/platform_augmentation.jl
+++ .pkg/platform_augmentation.jl
@@ -1,4 +1,6 @@
 using Base.BinaryPlatforms
+module __CUDA
+    using Base.BinaryPlatforms
 
 try
     using CUDA_Runtime_jll
@@ -60,8 +62,49 @@ function augment_platform!(platform::Platform)
     end
     BinaryPlatforms.set_compare_strategy!(platform, "cuda", cuda_comparison_strategy)
 
-    # store the fact that we're using a local CUDA toolkit, for debugging purposes
-    platform["cuda_local"] = string(local_toolkit)
-
     return platform
+end
+end
+
+# Can't use Preferences since we might be running this very early with a non-existing Manifest
+MPIPreferences_UUID = Base.UUID("3da0fdf6-3ccc-4f1b-acd9-58baa6c99267")
+const preferences = Base.get_preferences(MPIPreferences_UUID)
+
+# Keep logic in sync with MPIPreferences.jl
+function augment_mpi!(platform)
+    # Doesn't need to be `const` since we depend on MPIPreferences so we
+    # invalidate the cache when it changes.
+    # Note: MPIPreferences uses `Sys.iswindows()` without the `platform` argument.
+    binary = get(preferences, "binary", Sys.iswindows(platform) ? "MicrosoftMPI_jll" : "MPICH_jll")
+
+    abi = if binary == "system"
+        let abi = get(preferences, "abi", nothing)
+            if abi === nothing
+                error("MPIPreferences: Inconsistent state detected, binary set to system, but no ABI set.")
+            else
+                abi
+            end
+        end
+    elseif binary == "MicrosoftMPI_jll"
+        "MicrosoftMPI"
+    elseif binary == "MPICH_jll"
+        "MPICH"
+    elseif binary == "OpenMPI_jll"
+        "OpenMPI"
+    elseif binary == "MPItrampoline_jll"
+        "MPItrampoline"
+    else
+        error("Unknown binary: $binary")
+    end
+
+    if !haskey(platform, "mpi")
+        platform["mpi"] = abi
+    end
+    return platform
+end
+
+
+function augment_platform!(platform::Platform)
+    augment_mpi!(platform)
+    __CUDA.augment_platform!(platform)
 end
